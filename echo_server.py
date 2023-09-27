c521c3c23889bff14c6ce739970641949c0bbf91
KennyUA <74805627+KennyUA@users.noreply.github.com>
2023-09-27 15:58:36 -0600

    Bug Fix

--- echo_server.py
+++ echo_server.py
@@ -15,26 +15,26 @@ def handle_connection(conn,addr):
             print(data)
             conn.sendall(data)
 
-            # Start single threaded echo server
-            def start_server():
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.bind((HOST,PORT))
-                    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    s.listen()
-                    conn, addr = s.accept()
-                    handle_connection(conn, addr)
+# Start single threaded echo server
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST,PORT))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.listen()
+        conn, addr = s.accept()
+        handle_connection(conn, addr)
 
             
-            # Start multithreaded echo server
-            def start_threaded_server():
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.bind((HOST, PORT))
-                    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    s.listen(2) # Allow backlog of up too 2 connections => queue [waiting conn1, waiting conn2]
-                    while True:
-                        conn, addr = s.accept()
-                        thread = Thread(target=handle_connection, args=(conn, addr))
-                        thread.run()
+# Start multithreaded echo server
+def start_threaded_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.listen(2) # Allow backlog of up too 2 connections => queue [waiting conn1, waiting conn2]
+        while True:
+            conn, addr = s.accept()
+            thread = Thread(target=handle_connection, args=(conn, addr))
+            thread.run()
 
 
-            start_threaded_server()
+start_threaded_server()
